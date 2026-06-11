7063d62f3f0edc29700b866c03c4ae1c7b9cceff
Gerardo Cruz <gerardhdez123@gmail.com>
2026-06-11 00:02:03 -0600

    Kevin agregue las Funciones de los botones

--- pryProyecto/frmLogin.Designer.cs
+++ pryProyecto/frmLogin.Designer.cs
@@ -76,6 +76,7 @@
             btnAcceder.TabIndex = 3;
             btnAcceder.Text = "Acceder";
             btnAcceder.UseVisualStyleBackColor = true;
+            btnAcceder.Click += btnAcceder_Click;
             // 
             // btnSalir
             // 
@@ -86,6 +87,7 @@
             btnSalir.TabIndex = 4;
             btnSalir.Text = "Salir";
             btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
             // 
             // frmLogin
             // 
