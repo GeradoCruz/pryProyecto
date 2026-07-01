fad8e9ceee15f67d10e99a1bc9b763e06eae004e
Kevinlx-dotcom <20251005@uthh.edu.mx>
2026-07-01 10:51:48 -0600

    video 17-Kevin

--- pryProyecto/clsAlumnos.cs
+++ pryProyecto/clsAlumnos.cs
@@ -5,7 +5,6 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace pryProyecto
 {
